8196f679a280e284252e877fc68a6d3c5a3543f0
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-08-24 20:32:20 +0900

    feat : add example

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,3 +1,51 @@
+use std::path::Path;
+
+use gluesql::{core::data::Schema, prelude::Glue};
+
+pub enum LoggerType {
+    LogBack,
+}
+
+pub struct LogCustomSchema {
+    pub schema_type: LoggerType,
+    pub format: String,
+}
+
+impl LogCustomSchema {
+    pub fn new(schema_type: LoggerType, format: String) -> Self {
+        Self {
+            schema_type,
+            format,
+        }
+    }
+    pub fn to_schema(&self) -> Schema {
+        todo!()
+    }
+}
+
+// By specifying the log schema type, you determine which library's log format to parse.
+// The logic for parsing the log schema varies depending on the log schema type.
+//
+// The parsed results of the logs are used to generate schemas.
+// Provide the schema externally -> This includes LogBack configurations or file setup.
+// Insert into a storage engine for use.
+// Parse logs to generate schemas.
+
 fn main() {
-    core::run();
+    let log_format = "[%d{yyyy-MM-dd HH:mm:ss}:%-3relative] %-5level ${PID:-} --- [%15.15thread] %-40.40logger{36} : %msg%n".to_string();
+    let logger_type = LoggerType::LogBack;
+    let schema = LogCustomSchema::new(logger_type, log_format).to_schema();
+
+    let storage = LocalFileStorage::new(
+        "localhost".to_string(),
+        Path::new("/tmp").to_path_buf(),
+        schema,
+    );
+
+    let mut glue = Glue::new(storage);
+
+    let payloads = glue
+        .execute("SELECT * FROM logFile.2023-08-20.log;")
+        .await
+        .unwrap();
 }
--- storages/local-file-storage/src/lib.rs
+++ storages/local-file-storage/src/lib.rs
@@ -1,12 +1,18 @@
+use {
+    gluesql::core::data::Schema,
+    std::path::{Path, PathBuf},
+};
+
 mod store;
 
 pub struct LocalFileStorage {
     pub host: String,
-    pub path: String,
+    pub path: PathBuf,
+    pub schema: Schema,
 }
 
 impl LocalFileStorage {
-    pub fn new(host: String, path: String) -> Self {
-        Self { host, path }
+    pub fn new(host: String, path: PathBuf, schema: Schema) -> Self {
+        Self { host, path, schema }
     }
 }
